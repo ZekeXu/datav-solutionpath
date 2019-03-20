c5d45dc815b83c2bdec344d1e0dfd39f23f11518
XuLipeng <xulipeng@4paradigm.com>
2019-03-20 13:45:39 +0800

    Updates

---
+++ precache-manifest.f38e77d9552dee097392971fdab8b173.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8f65eb4e3473b9b9c819",
+    "url": "/datav-solutionpath/static/css/main.fba09f22.chunk.css"
+  },
+  {
+    "revision": "8f65eb4e3473b9b9c819",
+    "url": "/datav-solutionpath/static/js/main.8f65eb4e.chunk.js"
+  },
+  {
+    "revision": "526bf306f1a06b2bd7ea",
+    "url": "/datav-solutionpath/static/js/runtime~main.526bf306.js"
+  },
+  {
+    "revision": "44d45279c96e812f7d35",
+    "url": "/datav-solutionpath/static/css/2.314642ce.chunk.css"
+  },
+  {
+    "revision": "44d45279c96e812f7d35",
+    "url": "/datav-solutionpath/static/js/2.44d45279.chunk.js"
+  },
+  {
+    "revision": "9c37904f6caed1e045449a7d027c53b4",
+    "url": "/datav-solutionpath/index.html"
+  }
+];
